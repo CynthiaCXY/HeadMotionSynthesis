244d642faf57b512b6c2185cb97d5815843f7125
CynthiaCXY <xintongyu@xintongs-air.home>
2018-02-20 13:26:15 +0000

    Baseline model completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+""" Setup script for hms package. """
+
+from setuptools import setup
+
+setup(
+    name = "headMotionSynthesis",
+    author = "Xintong Yu",
+    description = ("Head Motion Synthesis using neural networks."),
+    url = "https://github.com/CynthiaCXY/HeadMotionSynthesis",
+    packages=['hms']
+)
